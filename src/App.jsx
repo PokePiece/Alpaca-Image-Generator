5fb1aa5f26ddca573387e5ad3eef1d0ccde38864
Dillon <DillonCarey333@@gmail.com>
2024-12-13 11:13:24 -0800

    Added the ability to adjust more features.

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ class Alpaca extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      eyeColor: 'black'
+      eyeColor: 'black',
+      furColor: "lightgrey"
     };
     this.handleEyeColorChange = this.handleEyeColorChange.bind(this);
   }
@@ -26,8 +27,15 @@ class Alpaca extends React.Component {
     this.setState({ eyeColor: color });
   };
 
+  handlefurColorChange = (color) => {
+    this.setState({furColor: color});
+  };
+
   render() {
+
+    
     return (
+      
       <div id="alpaca-program">
             <div id="alpaca-image" className="alpaca-entity">
               <div id="alpaca-head" className="alpaca-entity">
@@ -35,19 +43,33 @@ class Alpaca extends React.Component {
                 <div id="right-eye" className={`eye-${this.state.eyeColor}`}></div>
               </div>
               <div id="alpaca-body" className="alpaca-entity"></div>
-              <div id="alpaca-legs" className="alpaca-entity"></div>
+              <div id="alpaca-legs" className="alpaca-entity">
+                <div id="front-left-leg"></div>
+                <div id="front-right-leg"></div>
+                <div id="rear-left-leg"></div>
+                <div id="rear-right-leg"></div>
+              </div>
               <div id="alpaca-feet" className="alpaca-entity"></div>
             </div>
             <div id="interface">
-              <p>Can you read me? This is where the user will interact with
-                the interface to alter and adjust the Alpaca image.
-              </p>
+              <h2>Alpaca Adjustor Controls</h2>
+              <div id="adjust-alpaca">
+                <div id="eye-adjustors">
+                <h3>Adjust Eyes</h3>
               <button class="eye-btn" onClick={() => this.handleEyeColorChange('brown')}>Turn its eyes brown!</button>
               <button class="eye-btn" onClick={() => this.handleEyeColorChange('black')}>Turn its eyes black!</button>
               <button class="eye-btn" onClick={() => this.handleEyeColorChange('blue')}>Turn its eyes blue!</button>
+              </div>
+              <div id="fur-adjustors">
+                <h3>Adjust Fur</h3>
+                <button class="fur-btn" onClick={() => this.handlefurColorChange("blue")}>Give blue fur</button>
+                <button class="fur-btn" onClick={() => this.handlefurColorChange("grey")}>Give grey fur</button>
+              </div>
+              </div>
             </div>
             </div>
-    );
+            
+    ); 
   }
 }
 
